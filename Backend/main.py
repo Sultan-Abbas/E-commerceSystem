96a548ddb9321ce80947a7fe35a365916778302e
Muhammad Sultan Abbas <sultanabbas.3dartist@gmail.com>
2025-07-19 19:13:26 +0500

    Add user registration, login, and product APIs
    Implemented endpoints for user registration, login, greeting, adding products, and listing products in main.py. Added Pydantic schemas for user and product models in schemas.py to support request validation.

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,40 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+from schemas import RegisterUser,LoginUser,Product
 
 app = FastAPI()
-
+users_db = {}
+products_db = []
 
 @app.get("/")
 def read_root():
     return{"message":"Welcome to the Backend of Ecomerce System"}
+
+@app.get("/greet")
+def greet_user(name = Optional[str]=None):
+    if name:
+        return {"message":f"Hello!,{name}"}
+    return {"message":"Hello There!"}
+
+@app.post("/register")
+def register_user(user:RegisterUser):
+    if user in users_db:
+        return{"message":"You are already registered"}
+    else:
+        users_db[user.username]==user.password
+        
+@app.post("/login")
+def login(user: LoginUser):
+    if user not in users_db or user.username==user.password:
+        return{"message":"Invalid,user name or password in not correct"}
+    else:
+        return {"message": f"Welcome back, {user.username}!"}
+    
+@app.post("/add-product")
+def add_product(product: Product):
+    products_db.append(product)
+    return {"message": "Product added", "product": product}       
+@app.get("/showproducts")
+def get_products(products_db):
+    return {"products": products_db}
---
+++ Backend/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class RegisterUser(BaseModel):
+    username: str
+    password: str
+
+class LoginUser(BaseModel):
+    username: str
+    password: str
+
+class Product(BaseModel):
+    name: str
+    price: float
